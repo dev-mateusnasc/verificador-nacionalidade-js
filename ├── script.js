4fe8d333513f34a75ce7e592acab5eca6c2c6150
dev-mateusnasc <semhmateusn@gmail.com>
2026-03-06 19:08:27 -0300

    Update ├── script.js

--- ├── script.js
+++ ├── script.js
@@ -1,1 +1,11 @@
+<script>
+    function Verificar(){
+            var pais = document.getElementById('txtpais').value.toLowerCase()
+            var resultado = document.getElementById('resultado')
+            if (pais === 'brasil'){
+                resultado.innerHTML = 'Você é brasileiro(a)!'}
+                else {
+                    resultado.innerHTML = 'Você é estrangeiro(a)!🌎'}
+                }
 
+    </script>
